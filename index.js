382e741b989ecf89586fdeb762ed0d7964b21259
yamoonz <yamooonz@gmail.com>
2020-04-22 15:14:43 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-// Write your code here
+const num1= 31;
+const num2= 2;
+var multiply = num1*num2;
+console.log(multiply);
+
+var random= Math.floor(Math.random() * 10) + 1;
+
+//var mod= 
+
+//var max= 
+describe('mod', function() {
+    it("is generates a remainder that is equal to 4", function() {
+      expect(mod).to.eq(4)
+    });
+  });
+  describe('max', function() {
+    it("will return 20 as the highest number in the set", function() {
+      expect(max).to.eq(20)
+    });
+  });
